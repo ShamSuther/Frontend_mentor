ceb2a37eb3c13b87b1aa9060ec27a51ebe079f3f
ShamSuther <shamshivani1899@gmail.com>
2024-09-16 13:01:08 +0500

    trial-ball testing everything

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 import fluid, { extract } from 'fluid-tailwind'
 
-module.exports = {
+export default {
   content: {
     files: ["./*.html", "./scripts/*.js"],
     extract
   },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lime: "hsl(61, 70%, 52%)",
+        red: "hsl(4, 69%, 50%)",
+        white: {
+          DEFAULT: "hsl(0, 0%, 100%)",
+          100: "hsl(202, 86%, 94%)",
+          300: "hsl(203, 41%, 72%)",
+          500: "hsl(200, 26%, 54%)",
+          700: "hsl(200, 24%, 40%)",
+          900: "hsl(202, 55%, 16%)",
+        }
+      },
+    },
   },
   plugins: [fluid],
 }
